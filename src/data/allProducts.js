8fc64fe8c22403830f07f8d3c82ffadc8bc82784
FedeSagini <103580930+FedeSagini@users.noreply.github.com>
2023-02-08 14:41:54 -0300

    cambios

--- src/data/allProducts.js
+++ src/data/allProducts.js
@@ -1,70 +1,80 @@
 const allProducts = [
     {
         "id": 1,
-        "title": "PC Gamer Armada",
+        "title": "pc Gamer Armada",
+        "categoria": "pc",
         "description": "Intel Core I3 10100F + 8GB + GTX 1660 6Gb",
         "price": "25000",
         "pictureUrl": "https://s3-sa-east-1.amazonaws.com/saasargentina/VufovVQG0zBzgX4XprAY/miniatura",
     },
     {
         "id": 2,
-        "title": "PC Gamer Armada",
+        "title": "pc Gamer Armada",
+        "categoria": "pc",
         "description": "Intel Core I3 10400F + 8GB + GTX 1660 6Gb",
         "price": "25000",
         "pictureUrl": "https://s3-sa-east-1.amazonaws.com/saasargentina/VufovVQG0zBzgX4XprAY/miniatura"
     },
     {
         "id": 3,
-        "title": "PC Gamer Armada",
+        "title": "pc Gamer Armada",
+        "categoria": "pc",
         "description": "Intel Core I5 10100F + 8GB + GTX 1660 6Gb",
         "price": "25000",
         "pictureUrl": "https://s3-sa-east-1.amazonaws.com/saasargentina/VufovVQG0zBzgX4XprAY/miniatura"
     },
     {
         "id": 4,
-        "title": "PC Gamer Armada",
+        "title": "pc Gamer Armada",
+        "categoria": "pc",
         "description": "Intel Core I3 10100F + 8GB + GTX 2660 6Gb",
         "price": "25000",
         "pictureUrl": "https://s3-sa-east-1.amazonaws.com/saasargentina/VufovVQG0zBzgX4XprAY/miniatura"
     },
     {
         "id": 5,
-        "title": "PC Gamer Armada",
+        "title": "pc Gamer Armada",
+        "categoria": "pc",
         "description": "Intel Core I5 10400F + 16GB + GTX 1660 6Gb",
         "price": "25000",
         "pictureUrl": "https://s3-sa-east-1.amazonaws.com/saasargentina/VufovVQG0zBzgX4XprAY/miniatura"
     },
     {
         "id": 6,
-        "title": "PC Gamer Armada",
+        "title": "pc Gamer Armada",
+        "categoria": "pc",
         "description": "Intel Core I7 10100F + 8GB + GTX 1660 6Gb",
         "price": "25000",
         "pictureUrl": "https://s3-sa-east-1.amazonaws.com/saasargentina/VufovVQG0zBzgX4XprAY/miniatura"
     },
     {
         "id": 7,
-        "title": "PC Gamer Armada",
+        "title": "pc Gamer Armada",
+        "categoria": "pc",
         "description": "Intel Core I7 10100F + 8GB + GTX 3600 6Gb",
         "price": "25000",
         "pictureUrl": "https://s3-sa-east-1.amazonaws.com/saasargentina/VufovVQG0zBzgX4XprAY/miniatura"
     },
     {
         "id": 8,
-        "title": "PC Gamer Armada",
+        "title": "pc Gamer Armada",
+        "categoria": "pc",
         "description": "Intel Core I7 10900F + 16GB + GTX 3070 6Gb",
         "price": "25000",
         "pictureUrl": "https://s3-sa-east-1.amazonaws.com/saasargentina/VufovVQG0zBzgX4XprAY/miniatura"
     },
     {
         "id": 9,
-        "title": "PC Gamer Armada",
+        "title": "pc Gamer Armada",
+        "categoria": "pc",
         "description": "Intel Core I9 10100F + 8GB + GTX 3090 6Gb",
         "price": "250000",
         "pictureUrl": "https://s3-sa-east-1.amazonaws.com/saasargentina/VufovVQG0zBzgX4XprAY/miniatura"
     },
     {
         "id": 10,
-        "title": "PC Gamer Armada",
+        "title": "pc Gamer Armada",
+        "categoria": "pc",
         "description": "Intel Core I9 10900F + 32GB + GTX 3090 6Gb",
         "price": "2500000",
         "pictureUrl": "https://s3-sa-east-1.amazonaws.com/saasargentina/VufovVQG0zBzgX4XprAY/miniatura"
@@ -72,6 +82,7 @@ const allProducts = [
     {
         "id": 11,
         "title": "Notebook ASUS",
+        "categoria": "note",
         "description": "Notebook ASUS X515EA Intel I3 1115 G4 | RAM 4GB | SSD 256Gb | 15.6 | X515EA-",
         "price": "25",
         "pictureUrl": "https://s3-sa-east-1.amazonaws.com/saasargentina/Hfm8X6R3CvrIrc3OAnhv/miniatura"
@@ -79,6 +90,7 @@ const allProducts = [
     {
         "id": 12,
         "title": "Notebook ASUS",
+        "categoria": "note",
         "description": "Notebook ASUS X515EA Intel I3 1115 G4 | RAM 4GB | SSD 256Gb | 15.6 | X515EA-",
         "price": "25",
         "pictureUrl": "https://s3-sa-east-1.amazonaws.com/saasargentina/Hfm8X6R3CvrIrc3OAnhv/miniatura"
@@ -86,6 +98,7 @@ const allProducts = [
     {
         "id": 13,
         "title": "Notebook ASUS",
+        "categoria": "note",
         "description": "Notebook ASUS X515EA Intel I3 1115 G4 | RAM 4GB | SSD 256Gb | 15.6 | X515EA-",
         "price": "25",
         "pictureUrl": "https://s3-sa-east-1.amazonaws.com/saasargentina/Hfm8X6R3CvrIrc3OAnhv/miniatura"
@@ -93,6 +106,7 @@ const allProducts = [
     {
         "id": 14,
         "title": "Notebook ASUS",
+        "categoria": "note",
         "description": "Notebook ASUS X515EA Intel I3 1115 G4 | RAM 4GB | SSD 256Gb | 15.6 | X515EA-",
         "price": "25",
         "pictureUrl": "https://s3-sa-east-1.amazonaws.com/saasargentina/Hfm8X6R3CvrIrc3OAnhv/miniatura"
@@ -100,6 +114,7 @@ const allProducts = [
     {
         "id": 15,
         "title": "Notebook ASUS",
+        "categoria": "note",
         "description": "Notebook ASUS X515EA Intel I3 1115 G4 | RAM 4GB | SSD 256Gb | 15.6 | X515EA-",
         "price": "25",
         "pictureUrl": "https://s3-sa-east-1.amazonaws.com/saasargentina/Hfm8X6R3CvrIrc3OAnhv/miniatura"
@@ -107,6 +122,7 @@ const allProducts = [
     {
         "id": 16,
         "title": "Notebook ASUS",
+        "categoria": "note",
         "description": "Notebook ASUS X515EA Intel I3 1115 G4 | RAM 4GB | SSD 256Gb | 15.6 | X515EA-",
         "price": "25",
         "pictureUrl": "https://s3-sa-east-1.amazonaws.com/saasargentina/Hfm8X6R3CvrIrc3OAnhv/miniatura" 
@@ -114,6 +130,7 @@ const allProducts = [
     {
         "id": 17,
         "title": "Notebook ASUS",
+        "categoria": "note",
         "description": "Notebook ASUS X515EA Intel I3 1115 G4 | RAM 4GB | SSD 256Gb | 15.6 | X515EA-",
         "price": "25",
         "pictureUrl": "https://s3-sa-east-1.amazonaws.com/saasargentina/Hfm8X6R3CvrIrc3OAnhv/miniatura"
@@ -121,6 +138,7 @@ const allProducts = [
     {
         "id": 18,
         "title": "Notebook ASUS",
+        "categoria": "note",
         "description": "Notebook ASUS X515EA Intel I3 1115 G4 | RAM 4GB | SSD 256Gb | 15.6 | X515EA-",
         "price": "25",
         "pictureUrl": "https://s3-sa-east-1.amazonaws.com/saasargentina/Hfm8X6R3CvrIrc3OAnhv/miniatura"
@@ -128,6 +146,7 @@ const allProducts = [
     {
         "id": 19,
         "title": "Notebook ASUS",
+        "categoria": "note",
         "description": "Notebook ASUS X515EA Intel I3 1115 G4 | RAM 4GB | SSD 256Gb | 15.6 | X515EA-",
         "price": "25",
         "pictureUrl": "https://s3-sa-east-1.amazonaws.com/saasargentina/Hfm8X6R3CvrIrc3OAnhv/miniatura"
